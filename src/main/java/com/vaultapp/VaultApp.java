afb7c439b9ccf6a754cd61cc6c14cf255fe44fe6
FullOvellas <sergiotui1c@gmail.com>
2022-12-04 18:20:22 +0100

    chore: correct VaultApp location

--- src/main/java/com/vaultapp/VaultApp.java
+++ src/main/java/com/vaultapp/VaultApp.java
@@ -1,9 +1,11 @@
+package com.vaultapp;
+
 public class VaultApp {
 
     public static void main(String[] args) {
 
         String errMsg = "ERROR: argumentos no válidos\n\t" +
-                "Uso: java -jar VaultApp.jar [modo]\n" +
+                "Uso: java -jar com.vaultapp.VaultApp.jar [modo]\n" +
                 "Modos:\n\t" +
                 "--cli: aplicación en línea de comandos\n\t" +
                 "--gui: aplicación con cliente gráfico";
