1b6bd2f926f297fb4b128e0b28aa169b4d9b6cdc
FullOvellas <sergiotui1c@gmail.com>
2022-12-05 21:33:08 +0100

    fix: module-info statement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module proyecto.ud3.sergio.y.manuel {
     requires javafx.fxml;
     requires jakarta.persistence;
     requires atlantafx.base;
+    requires com.fasterxml.jackson.databind;
 
     exports com.vaultapp to javafx.graphics, javafx.fxml;
 }
