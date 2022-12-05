f01e61be9bebd9110d27b7da9d3463213343a143
FullOvellas <sergiotui1c@gmail.com>
2022-12-05 20:55:51 +0100

    feat(dao.api): added DAO for film API

---
+++ src/main/java/com/vaultapp/model/dao/api/FilmApiDao.java
@@ -0,0 +1,91 @@
+package com.vaultapp.model.dao.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.vaultapp.model.entities.Film;
+import com.vaultapp.model.pojo.film.ApiFilm;
+import com.vaultapp.model.pojo.film.FilmResultPage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmApiDao {
+
+    private final String SEARCH_URL = "https://api.themoviedb.org/3/search/movie?api_key=%s&query=%s&page=%d";
+    private final String MOVIE_URL = "https://api.themoviedb.org/3/movie/%d?api_key=%s";
+    private final String POSTER_URL = "https://image.tmdb.org/t/p/original/%s";
+    private final String API_KEY = "19ccdf01a305d5f5c3485958c90ef5d6";
+
+    private static FilmApiDao instance;
+
+    static {
+        instance = new FilmApiDao();
+    }
+
+    public static FilmApiDao getInstance() { return instance; }
+
+    public List<Film> searchByTitle(String title, int page) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        String url = String.format(SEARCH_URL, API_KEY ,title, page);
+        List<Film> films = new ArrayList<>();
+
+        try {
+            FilmResultPage resultPage = objectMapper.readValue(new URL(URLEncoder.encode(url, StandardCharsets.UTF_8)), FilmResultPage.class);
+            List<ApiFilm> pageFilms = resultPage.getPageFilms();
+            for (ApiFilm a : pageFilms) {
+                films.add(parseApiFilm(a));
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return films;
+    }
+
+    private Film parseApiFilm(ApiFilm apiFilm) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("uuuu-MM-dd");
+        int tmdbId = apiFilm.getId();
+        String title = apiFilm.getTitle();
+        String posterPath = apiFilm.getPosterPath();
+        List<String> genres = new ArrayList<>();
+        apiFilm.getGenres().forEach(genresItem -> genres.add(genresItem.getName()));
+        String overview = apiFilm.getOverview();
+        String originalTitle = apiFilm.getOriginalTitle();
+        List<String> productionCompanies = new ArrayList<>();
+        apiFilm.getProductionCompanies().forEach(productionCompany -> productionCompanies.add(productionCompany.getName()));
+        LocalDate releaseDate = LocalDate.parse(apiFilm.getReleaseDate(), formatter);
+        String tagline = apiFilm.getTagline();
+
+        return new Film(tmdbId,
+                title,
+                posterPath,
+                genres,
+                overview,
+                originalTitle,
+                productionCompanies,
+                releaseDate,
+                tagline);
+    }
+
+    public Film searchBytmdbId(int id) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ApiFilm film = null;
+        Film out;
+        String url = String.format(MOVIE_URL,id, API_KEY);
+
+        try {
+            film = objectMapper.readValue(new URL(URLEncoder.encode(url, StandardCharsets.UTF_8)), ApiFilm.class);
+            out = parseApiFilm(film);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return out;
+    }
+
+}
--- src/main/java/com/vaultapp/model/entities/Film.java
+++ src/main/java/com/vaultapp/model/entities/Film.java
@@ -2,6 +2,8 @@ package com.vaultapp.model.entities;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -11,6 +13,29 @@ public class Film {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private int tmdbId;
+    private String title;
+    private String posterPath;
+    @ElementCollection
+    private List<String> genres;
+    private String overview;
+    private String originalTitle;
+    @ElementCollection
+    private List<String> productionCompany;
+    private LocalDate releaseDate;
+    private String tagline;
+
+    public Film(int tmdbId, String title, String posterPath, List<String> genres, String overview, String originalTitle, List<String> productionCompany, LocalDate releaseDate, String tagline) {
+        this.tmdbId = tmdbId;
+        this.title = title;
+        this.posterPath = posterPath;
+        this.genres = genres;
+        this.overview = overview;
+        this.originalTitle = originalTitle;
+        this.productionCompany = productionCompany;
+        this.releaseDate = releaseDate;
+        this.tagline = tagline;
+    }
 
     public Long getId() {
         return id;
