f42a635b572d9e72c62cf8414be802e3637ce9c4
Manuel Landin Gomez <manuel.landin@outlook.es>
2022-12-08 11:51:37 +0100

    fix: small bugs in BookController

--- src/main/java/com/vaultapp/controller/BookController.java
+++ src/main/java/com/vaultapp/controller/BookController.java
@@ -2,7 +2,6 @@ package com.vaultapp.controller;
 
 import com.vaultapp.model.entities.Book;
 import com.vaultapp.model.repository.BookApiRepository;
-import com.vaultapp.model.repository.BookDbRepository;
 import com.vaultapp.view.cli.BookCliView;
 
 import java.util.List;
@@ -10,21 +9,15 @@ import java.util.Scanner;
 
 public class BookController {
     private static final Scanner sc = new Scanner(System.in);
-    private BookDbRepository bookRepository;
     private BookCliView bookCliView;
 
     public BookController() {
-        bookRepository = BookDbRepository.getInstance();
         bookCliView = new BookCliView();
     }
 
     public List<Book> bookFinder() {
         bookCliView.viewBookFinder();
         return BookApiRepository.getInstance().getAsList(sc.nextLine());
-    }
-
-    public BookDbRepository getBookRepository() {
-        return bookRepository;
     }
 
     public BookCliView getBookCliView() {
